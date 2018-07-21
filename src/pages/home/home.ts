2eebb81b0d88be458d1ae1562bb4d0fcf0dba171
Matthieu Drula <matthieu.drula.hs@gmail.com>
2018-07-21 11:01:17 +0200

    all working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Plugins } from '@capacitor/core';
+
+const { Modals } = Plugins;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) { }
 
-  constructor(public navCtrl: NavController) {
-
+  async showConfirm() {
+    await Plugins.Modals.confirm({
+      title: 'Capacitor integration test',
+      message: 'Is Capacitor working?'
+    });
   }
-
 }
